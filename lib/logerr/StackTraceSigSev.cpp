b14a79e07793df6697fe0a710f7a67a0509224e2
Nic Holthaus <nholthaus@gmail.com>
2020-06-27 16:19:22 -0400

    some more appinfo refactoring

--- lib/logerr/StackTraceSigSev.cpp
+++ lib/logerr/StackTraceSigSev.cpp
@@ -18,23 +18,23 @@ void stackTraceSigSev(int sig)
 #endif
 
 	QString time = "\n\nTIME:\n\n";
-	time += "    Start Time   : " + APPINFO::applicationStartTime.toString() + "\n";
+	time += "    Start Time   : " + APPINFO::applicationStartTime().toString() + "\n";
 	time += "    Crash Time   : " + QDateTime::currentDateTime().toString() + "\n";
 	time += "\n";
 
-	QString sDetails = QString("%1 Crashed! :'(").arg(qApp->applicationName()) + time + SystemDetails() + QString("STACK TRACE:\n\n") + trace;
+	QString sDetails = QString("%1 Crashed! :'(").arg(APPINFO::name()) + time + SystemDetails() + QString("STACK TRACE:\n\n") + trace;
 	LOGERR << sDetails.toLocal8Bit().constData() << std::endl;
 
 	// make sure the directory exists
 	QDir dir;
-	dir.mkdir(APPINFO::crashDumpDir);
+	dir.mkdir(APPINFO::crashDumpDir());
 
 	QString crashdumpFileName = QString("crashdump-") + QDateTime::currentDateTime().toString(Qt::ISODate).remove(':') + ".txt";
 	if (!qApp->applicationName().isEmpty())
 		crashdumpFileName.prepend(qApp->applicationName() + '-');
 
 	// write a dedicated crash dump file too for good measure
-	QFile crashDumpFile(APPINFO::crashDumpDir + '/' + crashdumpFileName);
+	QFile crashDumpFile(APPINFO::crashDumpDir() + '/' + crashdumpFileName);
 	crashDumpFile.open(QIODevice::WriteOnly);
 	crashDumpFile.write(sDetails.toLocal8Bit());
 	crashDumpFile.close();
