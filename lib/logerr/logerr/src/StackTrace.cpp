c40d81e9c738e02555f1a5e9320f0724e20efa28
Nic Holthaus <nholthaus@gmail.com>
2020-10-16 16:12:35 -0400

    WIP: time to test on windows

--- lib/logerr/logerr/src/StackTrace.cpp
+++ lib/logerr/logerr/src/StackTrace.cpp
@@ -17,10 +17,12 @@
 #pragma comment(lib, "dbghelp.lib")
 #pragma warning(pop)
 #else
-#include <cxxabi.h>
 #include <backtraceSymbols.h>
-#include <stdlib.h>
+#include <cxxabi.h>
+#include <execinfo.h>
 #endif    // _MSC_VER
+
+#include <QDebug>
 
 //--------------------------------------------------------------------------------------------------
 //	StackTrace ( public )
@@ -66,11 +68,11 @@ StackTrace::StackTrace(unsigned int ignore /*= 0*/)
 			lineNumber = 0;
 		}
 
-		value << std::right << std::setw(5)<< "["
-		      << std::left << std::setw(0) << (i - 1)
+		value << std::right << std::setw(5) << "["
+		      << std::left << std::dec << std::setw(frames / 10 + 1) << (i - 1)
 		      << std::left << std::setw(4) << "]"
 		      << std::left << std::setw(0) << "0x"
-		      << std::right << std::setw(16) << std::setfill('0') << symbol->Address
+		      << std::right << std::hex << std::setw(16) << std::setfill('0') << symbol->Address
 		      << std::left << std::setw(0) << ": "
 		      << std::left << std::setw(60) << std::setfill(' ') << symbol->Name
 		      << std::left << std::setw(0) << "| "
@@ -83,117 +85,89 @@ StackTrace::StackTrace(unsigned int ignore /*= 0*/)
 
 #else
 	// storage array for stack trace address data
-	void* addrlist[MAX_FRAMES];
+	void* trace[MAX_FRAMES];
 
 	// retrieve current stack addresses
-	int addrlen = backtrace(addrlist, sizeof(addrlist) / sizeof(void*));
+	int frames = backtrace(trace, sizeof(trace) / sizeof(void*));
 
-	if (addrlen == 0)
+	if (frames == 0)
 	{
 		m_value.append("<empty, possibly corrupt>\n");
+		return;
 	}
 
-	// resolve addresses into strings containing "filename(function+address)",
-	// this array must be free()-ed
-	char** symbollist = backtrace_symbols(addrlist, addrlen);
+	// resolve addresses into strings containing
+	auto&& symbols = backtraceSymbols(trace, frames);
 
 	// allocate string which will be filled with the de-mangled function name
-	char* funcname = new char[FUNCTION_NAME_SIZE];
+	char* demangledFunctionName = new char[FUNCTION_NAME_SIZE];
 
 	std::ostringstream value;
 
 	// iterate over the returned symbol lines. skip the first, it is the
 	// address of this function.
-	for (int i = 1 + ignore; i < addrlen; i++)
+	for (int i = 1 + ignore; i < frames; i++)
 	{
-		char *begin_name = 0, *begin_offset = 0, *end_offset = 0;
+		auto& [filename, functionName] = symbols[i];
+		qDebug() << filename.data() << "," << functionName.data();
 
-		// find parentheses and +address offset surrounding the mangled name:
-		// ./module(function+0x15c) [0x8048a6d]
-		for (char* p = symbollist[i]; *p; ++p)
+		int    demanglerStatus;
+		size_t demangledNameLength;
+
+		// if you see "Error: invalid pointer" here you probably need to`
+		// increase the size of FUNCTION_NAME_SIZE.
+		char* ret = abi::__cxa_demangle(functionName.data(),
+		                                demangledFunctionName,
+		                                &demangledNameLength,
+		                                &demanglerStatus);
+
+		if (demanglerStatus == 0)
 		{
-			if (*p == '(')
-				begin_name = p;
-			else if (*p == '+')
-				begin_offset = p;
-			else if (*p == ')' && begin_offset)
-			{
-				end_offset = p;
-				break;
-			}
+			functionName = ret;    // use possibly realloc()-ed string
+			value << std::right << std::setw(5) << "["
+			      << std::right << std::dec << std::setw(frames / 10 + 1) << (i - ignore)
+			      << std::left << std::setw(4) << "]"
+			      << std::left << std::setw(0) << "0x"
+			      << std::right << std::hex << std::setw(16) << std::setfill('0') << (unsigned long long) trace[i]
+			      << std::left << std::setw(0) << ": "
+			      << std::left << std::setw(60) << std::setfill(' ') << functionName
+			      << std::left << std::setw(0) << "| ";
 		}
-
-		if (begin_name && begin_offset && end_offset && begin_name < begin_offset)
+		else if (!functionName.empty())
 		{
-			*begin_name++   = '\0';
-			*begin_offset++ = '\0';
-			*end_offset     = '\0';
-
-			// mangled name is now in [begin_name, begin_offset) and caller
-			// offset in [begin_offset, end_offset). now apply
-			// __cxa_demangle():
-
-			int    status;
-			size_t nameLength;
-
-			// if you see "Error: invalid pointer" here you probably need to`
-			// increase the size of FUNCTION_NAME_SIZE.
-			char* ret = abi::__cxa_demangle(begin_name,
-			                                funcname,
-			                                &nameLength,
-			                                &status);
-
-			if (status == 0)
-			{
-				funcname = ret;    // use possibly realloc()-ed string
-				value << std::right << std::setw(5)<< "["
-				      << std::left << std::setw(0) << (i - ignore)
-				      << std::left << std::setw(4) << "]"
-				      << std::left << std::setw(0) << "0x"
-				      << std::right << std::setw(16) << std::setfill('0') << (unsigned long long) addrlist[i]
-				      << std::left << std::setw(0) << ": "
-				      << std::left << std::setw(60) << std::setfill(' ') << funcname
-				      << std::left << std::setw(0) << "| ";
-			}
-			else
-			{
-				// de-mangling failed. Output function name as a C function with
-				// no arguments.
-				value << std::right << std::setw(5)<< "["
-				      << std::left << std::setw(0) << (i - ignore)
-				      << std::left << std::setw(4) << "]"
-				      << std::left << std::setw(0) << "0x"
-				      << std::right << std::setw(16) << std::setfill('0') << (unsigned long long) addrlist[i]
-				      << std::left << std::setw(0) << ": "
-				      << std::left << std::setw(60) << std::setfill(' ') << begin_name
-				      << std::left << std::setw(0) << "| ";
-			}
+			// de-mangling failed, but there is a symbol name. Output function name as a C function with
+			// no arguments.
+			value << std::right << std::setw(5) << "["
+			      << std::right << std::dec << std::setw(frames / 10 + 1) << (i - ignore)
+			      << std::left << std::setw(4) << "]"
+			      << std::left << std::setw(0) << "0x"
+			      << std::right << std::hex << std::setw(16) << std::setfill('0') << (unsigned long long) trace[i]
+			      << std::left << std::setw(0) << ": "
+			      << std::left << std::setw(60) << std::setfill(' ') << functionName
+			      << std::left << std::setw(0) << "| ";
 		}
 		else
 		{
-			// couldn't parse the line? print the whole line.
-			value << std::right << std::setw(5)<< "["
-			      << std::left << std::setw(0) << (i - ignore)
+			// No symbol name. Print the module instead
+			value << std::right << std::setw(5) << "["
+			      << std::right << std::dec << std::setw(frames / 10 + 1) << (i - ignore)
 			      << std::left << std::setw(4) << "]"
 			      << std::left << std::setw(0) << "0x"
-			      << std::right << std::setw(16) << std::setfill('0') << (unsigned long long) addrlist[i]
+			      << std::right << std::hex << std::setw(16) << std::setfill('0') << (unsigned long long) trace[i]
 			      << std::left << std::setw(0) << ": "
-			      << std::left << std::setw(60) << std::setfill(' ') << symbollist[i]
+			      << std::left << std::setw(60) << std::setfill(' ') << "<no symbol found>"
 			      << std::left << std::setw(0) << "| ";
 		}
 
-		// get the file name associated with the symbol
-//		char** bts = backtraceSymbols(&addrlist[i], 1);
-//		value << bts[0] << std::endl;
-//		free(bts);
+		value << filename << std::endl;
 	}
 
 	// store values
 	m_value = value.str();
 
 	// Clean up
-	delete[] funcname;
-	free(symbollist);
+	delete[] demangledFunctionName;
+
 #endif
 }
 
